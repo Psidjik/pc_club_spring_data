08a7421be80fd1f60f9af5459bd92be322c3edb9
Admin <Psidjik>
2023-07-17 19:03:01 +0300

    It is a base!

---
+++ src/main/java/com/example/pc_club_spring_data/PcClubSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.pc_club_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcClubSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcClubSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pc_club_spring_data/PcClubSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pc_club_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcClubSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
